2d1645f68571bb6f091a28e6dafd5a773ad0cf55
Muhammad Dimas Adijanto <adijanto.205026@mhs.its.ac.id>
2022-11-14 04:36:32 +0700

    hiding support and action bar

--- app/src/main/java/com/example/androidfinalproject/SplashScreen.kt
+++ app/src/main/java/com/example/androidfinalproject/SplashScreen.kt
@@ -1,22 +1,24 @@
 package com.example.androidfinalproject
 
 import android.content.Intent
+import android.os.Build
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.os.Looper
 import android.view.View
 import android.view.Window
 import android.view.WindowManager
+import androidx.annotation.RequiresApi
 import java.util.logging.Handler
 
 class SplashScreen : AppCompatActivity() {
+    @RequiresApi(Build.VERSION_CODES.R)
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-
-        window.setFlags(
-            WindowManager.LayoutParams.FLAG_FULLSCREEN,
-            WindowManager.LayoutParams.FLAG_FULLSCREEN
+        window.decorView.windowInsetsController!!.hide(
+            android.view.WindowInsets.Type.statusBars()
         )
+        supportActionBar?.hide()
         setContentView(R.layout.activity_splash_screen)
 
 
